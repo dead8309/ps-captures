c4dd7461293262dadde85017942be518ce84e69f
Vaibhav Raj <vaibhavmonu8309@gmail.com>
2025-10-19 00:01:00 +0530

    make black default colorscheme

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
  export default function Page() {
    return (
-     <main className="min-h-dvh bg-black flex flex-col">
+     <main className="min-h-dvh flex flex-col">
        <div className="flex-1 flex flex-col">
          <CapturesBrowser />
        </div>
